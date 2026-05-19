e928b12c60178343b4271d5afa9e5c0afe56dc5f
AClon314 <30747832+AClon314@users.noreply.github.com>
2026-05-19 14:16:54 +0800

    install, 增加env动态变量

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 declare module "*.mjs";
+
+interface ImportMetaEnv {
+  readonly MIRROR_CONDA?: string;
+  readonly UV_DEFAULT_INDEX?: `${string}/simple`;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
