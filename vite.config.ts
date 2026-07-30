c5a24270a0de08cdae93b7924494f790d54eb7dd
AClon314 <30747832+AClon314@users.noreply.github.com>
2026-07-30 14:29:52 +0800

    add oxc, load .env for allowedHost

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,38 @@ import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	resolve: {
-		alias: {
-			$lib: fileURLToPath(new URL('./src/lib', import.meta.url))
-		}
-	},
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
-				experimental: { async: true }
-			},
+export default defineConfig(({ mode }) => {
+  const { allowedHost } = loadEnv(mode, process.cwd(), '');
+  return {
+    server: allowedHost ? { allowedHosts: [allowedHost] } : undefined,
+    resolve: {
+      alias: {
+        $lib: fileURLToPath(new URL('./src/lib', import.meta.url))
+      }
+    },
+    plugins: [
+      tailwindcss(),
+      sveltekit({
+        compilerOptions: {
+          // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+          runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+          experimental: { async: true }
+        },
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter(),
-			experimental: { remoteFunctions: true }
-		}),
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        adapter: adapter(),
+        experimental: { remoteFunctions: true }
+      }),
 
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide',
-			emitTsDeclarations: true
-		})
-	]
+      paraglideVitePlugin({
+        project: './project.inlang',
+        outdir: './src/lib/paraglide',
+        emitTsDeclarations: true
+      })
+    ]
+  };
 });
