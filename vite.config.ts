0bbfd0052adfbea323ff5a12f589260a18b61cf1
AClon314 <30747832+AClon314@users.noreply.github.com>
2026-09-06 22:10:31 +0800

    switch to cf adapter for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import tailwindcss from "@tailwindcss/vite";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
@@ -30,10 +30,7 @@ export default defineConfig(({ mode }) => {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-          pages: "build",
-          assets: "build",
-        }),
+        adapter: adapter(),
         experimental: { remoteFunctions: true },
       }),
 
